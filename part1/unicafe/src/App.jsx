d7bc1195e00e8899e67ee3c16a541b08242fc580
adriafigueres <adria.figueres@m10c.com>
2023-09-10 17:43:49 +0100

    feat: 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,13 +4,32 @@ const App = () => {
     const [good, setGood]       = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad]         = useState(0)
+    const [all, setAll]         = useState(0)
+    const [average, setAverage] = useState(0)
 
-    const increaseGood      = () => setGood(good+1)
-    const increaseNeutral   = () => setNeutral(neutral+1)
-    const increaseBad       = () => setBad(bad+1)
+    const increaseGood = () => {
+        setGood(good+1)
+        setAll(all+1)
+        setAverage(average+1)
+    }
+    const increaseNeutral = () => {
+        setNeutral(neutral+1)
+        setAll(all+1)
+    }
+    const increaseBad = () => {
+        setBad(bad+1)
+        setAll(all+1)
+        setAverage(average-1)
+    }
 
     const Button    = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
-    const Display   = ({counter, text}) => <div>{text}: {counter} votes</div>
+    const Display   = ({counter, text}) => {
+        return (all === 0) 
+            ? <div>{text}: 0</div> 
+            : (text === 'Positive')
+                ? <div>{text}: {counter*100}%</div>
+                : <div>{text}: {counter}</div>
+    }
 
     return (
         <div>
@@ -31,6 +50,9 @@ const App = () => {
             <Display text={'Good'} counter={good}/>
             <Display text={'Neutral'} counter={neutral}/>
             <Display text={'Bad'} counter={bad}/>
+            <Display text={'All'} counter={all}/>
+            <Display text={'Average'} counter={average/all}/>
+            <Display text={'Positive'} counter={good/all}/>
         </div>
 
             
